71a4b62a17d7bda78b9c8fa47b32cafd2f0a8ccd
Brian Reisman <brianreisman@protonmail.com>
2020-12-19 21:55:04 -0500

    reconfigured component folder structure. Added component lifecycle methods to optimize performance

--- burger-builder-starting-setup/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-starting-setup/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,41 @@
 import React from "react";
-import Aux from "../../../hoc/Auxiliary";
-import Button from '../../UI/Button/Button';
+import Aux from "../../../hoc/Auxiliary/Auxiliary";
+import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+class OrderSummary extends React.Component {
+  componentWillUpdate(){
+    console.log('{Order Summery}, WillUpdate')
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+
     return (
-      <li key={igKey} >
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+      <Aux>
+        <h3>Your order is ready!</h3>
+        <p>A delicous burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Your total comes to: ${this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+
+        <Button btnType={"Danger"} clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType={"Success"} clicked={this.props.purchaseContinue}>
+          CONTINUE
+        </Button>
+      </Aux>
     );
-  });
+  }
+}
 
-  return (
-    <Aux>
-      <h3>Your order is ready!</h3>
-      <p>A delicous burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Your total comes to: ${props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-
-      <Button btnType={'Danger'} clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType={'Success'} clicked={props.purchaseContinue}>CONTINUE</Button>
-    </Aux>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;

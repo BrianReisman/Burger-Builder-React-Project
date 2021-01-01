d271878f480e3eb378f6466bc98b8d27f3e97ebe
BrianReisman <brianreisman@protonmail.com>
2020-12-31 23:25:13 -0500

    added a spinner while axios post is taking place

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -23,6 +25,7 @@ class BurgerBuilder extends Component {
     totalPrice: 4,
     purchaseable: false,
     purchasing: false,
+    loading: false,
   };
 
   updatePurchaseState(ingredients) {
@@ -81,7 +84,34 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
+    // alert("You continue!");
+    this.setState({loading: true})
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, //for ecomm, recalc price on server to prevent people from tampering with it.
+      customer: {
+        name: 'B',
+        address: {
+          street: '1234 test Ave',
+          zip: '1234',
+          country: 'chicken',
+        },
+        email: 'asdf@asdf.com',
+      },
+      deliveryMethod: 'fastest',
+    }
+    axios
+      .post('/orders.json', order) //adding .json is needed for firebase specifically. The path you want to send your data to
+      .then(res => {
+        console.log(res)
+        this.setState({loading: false, purchasing: false})
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState({loading: false, purchasing: false})
+      })
+
+
   };
 
   render() {
@@ -92,31 +122,36 @@ class BurgerBuilder extends Component {
     for (let key in disableInfo) {
       disableInfo[key] = disableInfo[key] <= 0; //2)this updates the copied state objects with boolean values.
     }
+    let orderSummary = <OrderSummary
+      purchaseCancelled={this.purchaseCancelHandler}
+      purchaseContinue={this.purchaseContinueHandler}
+      ingredients={this.state.ingredients}
+      price={this.state.totalPrice}
+    />
 
-    return (
-      <Aux>
-        <Modal
-          show={this.state.purchasing}
-          modalClosed={this.purchaseCancelHandler}
-        >
-          <OrderSummary
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinue={this.purchaseContinueHandler}
-            ingredients={this.state.ingredients}
+    if (this.state.loading){
+      orderSummary = <Spinner/>
+    }
+
+      return (
+        <Aux>
+          <Modal
+            show={this.state.purchasing}
+            modalClosed={this.purchaseCancelHandler}
+          >
+            {orderSummary}
+          </Modal>
+          <Burger ingredients={this.state.ingredients} />
+          <BuildControls
+            ingredientAdded={this.addIngredientHandler}
+            ingredientRemoved={this.removeIngredientHandler}
+            disable={disableInfo} //3) we are now passing the state containing booleans under the name 'disable'
             price={this.state.totalPrice}
+            purchaseable={this.state.purchaseable}
+            ordered={this.purchaseHandler}
           />
-        </Modal>
-        <Burger ingredients={this.state.ingredients} />
-        <BuildControls
-          ingredientAdded={this.addIngredientHandler}
-          ingredientRemoved={this.removeIngredientHandler}
-          disable={disableInfo} //3) we are now passing the state containing booleans under the name 'disable'
-          price={this.state.totalPrice}
-          purchaseable={this.state.purchaseable}
-          ordered={this.purchaseHandler}
-        />
-      </Aux>
-    );
+        </Aux>
+      );
   }
 }
 
